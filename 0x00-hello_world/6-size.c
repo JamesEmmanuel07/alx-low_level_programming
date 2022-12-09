9c84ca2e3577dcbd531a5dee0ec7355adf7ce6dd
James Emmanuel <je6973013@gmail.com>
2022-12-09 13:40:00 -0800

    LLL

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
-
 /**
- * main - Prints out strings
- * printf is a function that prints out strings to
- * stdout
- * Return: All good
+ * main - Entry
+ * Return: Always 0
  */
-
-int main()
+int main(void)
 {
-	char a;
-	int b;
-	double c;
-
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of an int: %lu bytes(s)\n", (unsigned long)sizeof(b)); 
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(c));
-
-	return(0)
+	printf("Size of an char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int)); 
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
+	return (0);
 }
