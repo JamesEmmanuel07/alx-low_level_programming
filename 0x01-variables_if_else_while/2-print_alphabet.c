ae327618c47e3e16c499b22093063d238c276e59
James Emmanuel <je6973013@gmail.com>
2022-12-10 03:53:43 -0800

    lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *  main - Entry point
+ *
+ *  Return: 0 Always
+ */
+int main(void)
+{
+	char lc;
+
+	for
+	(lc = 'a';
+	 lc <= 'z';
+	 lc++);
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
