0b2cf15a5deddf42ed904789d6aedf6672b908b4
James Emmanuel <je6973013@gmail.com>
2022-12-12 04:47:07 -0800

    Advanced Task 1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: display digits and ,
+ *
+ * Return: Always 0 (successful)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
