721fe7318071233ea32cc74d1ff3749e6f9d01f8
James Emmanuel <je6973013@gmail.com>
2022-12-15 07:55:52 -0800

    print alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints a - z 10 times
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int x;
+	char c;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
