eb054e9bf62bbeba0297a2f50ce3d1ee7e19189a
James Emmanuel <je6973013@gmail.com>
2022-12-16 12:38:41 -0800

    task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
  * _isdigit - check for ints
+ * @c: to be checked
  *
- * Return 1 || 0
+ * Return: 1 || 0
  */
 int _isdigit(int c)
 {
-	if (c >= '0' && <= '9')
+	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
