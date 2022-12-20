131adf712b14b9dcf5af24822e2e49247db40ad6
James Emmanuel <je6973013@gmail.com>
2022-12-20 12:21:24 -0800

    Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,19 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * _strlen - returns length odf string
- * @s: str
- * Return: 0
+ * _strlen - function that returns the length of a string.
+ * @s: string of length
+ * Return: return length as integer
  */
 
-int _strlen(char *s0
+int _strlen(char *s)
 {
-	return (strlen(s));
+	int len = 0;
+
+	while (*(s + len) != '\0')
+	{
+		len++;
+	}
+	return (len);
 }
